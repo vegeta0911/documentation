d20288d209a67c2769e90184101f3716306ac00d
vegeta0911 <67876957+vegeta0911@users.noreply.github.com>
2025-06-16 23:19:57 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ var docMenu = [
 	divider: true
   },
   {
-		icon: "fa-2x fab fa-github",
+		icon: "fab fa-github",
 		fr_FR: "Github",
 		link: "https://github.com/Vegeta0911/"
   }, 
@@ -11,7 +11,7 @@ var docMenu = [
 	divider: true
   },
   {
-	        icon: "fa-2x fab fa-book",
+	        icon: "fab fa-book",
 		fr_FR: "Plugins",
 	        link: "https://vegeta0911.github.io/documentation/plugins/fr_FR/"
   },
