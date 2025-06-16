ec0e0fe5c2dfd88a336f7a7e56fe37abc0c72247
vegeta0911 <67876957+vegeta0911@users.noreply.github.com>
2025-06-16 18:54:00 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -35,7 +35,7 @@ var docMenu = [
   {
 	fr_FR: "Faire un don ❤️",
 	en_US: "Make a donation ❤️",
-	link: "https://www.paypal.com/donate/?hosted_button_id=X97RV78YRSAZY",
+	link: "https://www.paypal.com/donate/?hosted_button_id=4SZ67QSJ35H2E",
 	target: "_blank",
 	icon: "fa-2x fab fa-paypal"
   },
